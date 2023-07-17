fdb0ef67d30f153d0a3212f7a4c007e126b4e9d9
Artem <moiseev@physics.msu.ru>
2023-07-17 12:13:10 +0300

    Initial Commit

---
+++ WeSplit(100daysSU)/WeSplit_100daysSU_App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit_100daysSU_App.swift
+//  WeSplit(100daysSU)
+//
+//  Created by Artem on 17.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit_100daysSU_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
